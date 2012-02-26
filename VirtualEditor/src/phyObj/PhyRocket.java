837b45523030481f64db3be2feeb4dbba1508686
fromtheofficeofthepresident@gmail.com <fromtheofficeofthepresident@gmail.com@a4d2d8b6-0870-f7fe-9744-1b6032ed9804>
2012-02-26 12:46:04 +0000

    sad panda :(

--- VirtualEditor/src/phyObj/PhyRocket.java
+++ VirtualEditor/src/phyObj/PhyRocket.java
@@ -1,5 +1,26 @@
 package phyObj;
 
-public class PhyRocket { 
+public class PhyRocket extends PhysicsObject { 
 
+
+	PhyTriangle my_leftWing;
+	
+	PhyTriangle my_rightWinTriangle;
+	
+	PhyTriangle my_top;
+	
+	PhyRectangle my_body;
+	
+		
+	public PhyRocket(){
+//		my_leftWing = new PhyTriangle(size);
+//		my_rightWinTriangle = new PhyTriangle(size);
+//		my_top = new PhyTriangle(size);
+		my_body = new PhyRectangle();
+	}
+	
+	
+	
+	
+	
 }
