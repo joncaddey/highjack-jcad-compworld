009f2fb088143284661e3a5d7b26aa1938c352ed
joncaddey <joncaddey@gmail.com>
2012-03-02 08:29:44 +0000

    Messin' around.  Maybe fix scenegraphs so they can accept arbitrary vertices and colors.  GL_TRIANGLE_FAN

--- VirtualEditor/src/main/Triangle.java
+++ VirtualEditor/src/main/Triangle.java
@@ -11,24 +11,27 @@ import javax.media.opengl.GLAutoDrawable;
  */
 public class Triangle extends SceneGraphNode {
 	public static final float SIN_60 = (float)Math.sin(Math.PI / 3);
+	private static final float[] VERTICES_EQ_TRIANGLE = {-.5f, -SIN_60 / 3, .5f, -SIN_60 / 3, 0, SIN_60 * 2 / 3};
 
+	private final float[] vertices;
 	public Triangle() {
 		this(true);
 	}
 	
 	public Triangle(boolean pickable) {
 		super(pickable);
+		vertices = VERTICES_EQ_TRIANGLE;
 	}
 	
 	public void renderGeometry(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
 		gl.glBegin(GL.GL_TRIANGLES);
 		gl.glColor3f(1, .8f, 0);
-		gl.glVertex2f(-.5f, -SIN_60 / 3);
+		gl.glVertex2f(vertices[0], vertices[1]);
 		gl.glColor3f(1, .7f, 0);
-		gl.glVertex2f(0, SIN_60 * 2 / 3);
+		gl.glVertex2f(vertices[2], vertices[3]);
 		gl.glColor3f(1, .6f, 0);
-		gl.glVertex2f(.5f, -SIN_60 / 3);
+		gl.glVertex2f(vertices[4], vertices[5]);
 		gl.glEnd();
 	}
 }
